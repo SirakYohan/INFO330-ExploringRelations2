8c85ae9b4be130b49eb9911b56a4098fff0b3648
Sirak Yohannes <102786475+SirakYohan@users.noreply.github.com>
2023-04-11 23:55:24 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,5 +1,5 @@
 -- What was the most purchased track of 2013?
-Select *
+Select TrackId.invoice_items, TrackId.tracks, Names
 From invoice_items, tracks
 Group BY TrackId.invoice_items, TrackId.tracks, Names
 Order By Sum(TrackId)
