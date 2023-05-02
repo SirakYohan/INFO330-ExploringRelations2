8f8f74919b9ab86a10c558915a7bd107e8df919d
Sirak Yohannes <102786475+SirakYohan@users.noreply.github.com>
2023-05-02 10:43:29 -0700

    Update question-3.sql

--- question-3.sql
+++ question-3.sql
@@ -1,4 +1,8 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
 Select Composer, Name, UnitPrice, InvoiceId, InvoiceDate, total, Quantity
-From artists, tracks, invoices, invoice_items
+  From tracks
+    join albums on tracks.alubumid = albums.albumsid
+    join artists on album.artistid = artist.artistid
+    join invoices_items on invoice_items.trackid = tracks.trackid
+    join invoices on invoice_items.invoiceid = invoices.invoiceid
 
